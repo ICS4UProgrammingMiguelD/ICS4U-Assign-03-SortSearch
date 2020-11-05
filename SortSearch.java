a1afc872e2ddeb203667165821ac4ec4ca584731
Ubuntu <ubuntu@ip-172-31-91-5.ec2.internal>
2020-11-05 00:49:05 +0000

    Almost working

--- SortSearch.java
+++ SortSearch.java
@@ -6,39 +6,89 @@ import java.util.Scanner;
 
 * @author  Miguel Duarte
 * @version 1.0
-* @since   2020-10-22
+* @since   2020-10-28
 */
 public class SortSearch {
+  
   public static void main(String[] args) {
     
-    String output = "";
+    // Create an array with 250 random numbers
     int[] randomNumArray = generaterandomnumbers();
+    
+    // Read the number to find in the array from the user
+    
+    // Create input file
+    File inputFile = new File("./input.txt");
+    FileWriter myWriter = new FileWriter("./input.txt");.
+    
     for (int i = 0; i < randomNumArray.length; i++) {
       String tempString = String.valueOf(randomNumArray[i]);
-      output += tempString + "\r\n";
+      output += tempString + " ";
     }
     writetotxt(output);
+    
+    BubbleSort ob = new BubbleSort(); 
+    ob.bubbleSort(randomNumArray); 
+    System.out.println("Sorted array"); 
+    ob.printArray(randomNumArray); 
   }
   
   /**.
-   * 
+   * Function: GnerateRandomNumbers
+   * Input: None
+   * Output: An array of 250 random numbers
+   * This function creates an array that is 250 cells
+   * long and populates it with random numbers. It then
+   * returns the array.
   */
   public static int[] generaterandomnumbers() {
     
-    // Randomly generate 250 values
+    // Create random object
     Random randomNum  = new Random();
     
+    // Create an array with a length of 250
     int[] randomNumArray = new int[250];
     
+    // create writer object
+    
+    // Populate the array with random numbers
     for (int index = 0; index < 250; index++) {
       
       // Obtain a number between [1 - 99]
       int computerNumber = randomNum.nextInt(100);
       
+      // Put the random number into the array at the index
       randomNumArray[index] = computerNumber;
-      System.out.println(randomNumArray[index]);
+      // System.out.println(randomNumArray[index]);
     }
     return randomNumArray;
+  }
+  
+  /**.
+   * 
+  */
+  public static int[] bubblesort() {
+    
+    int n = randomNumArray.length;
+    for (int i = 0; i < n - 1; i++)
+      for (int j = 0; j < n - i - 1; j++)
+        if (randomNumArray[j] > randomNumArray[j + 1]) {
+          
+          //Swap
+          int temp = randomNumArray[j];
+          randomNumArray[j] = randomNumArray[j + 1];
+          randomNumArray[j + 1] = temp;
+        }
+  }
+  
+  /**.
+   * 
+  */
+  void printArray(int arr[]) {
+    int n = randomNumArray.length;
+    for (int i = 0; i < n; ++1)
+      System.out.print(randomNumArray[i] + " ");
+    System.out.println();
   }
   
   /**.
